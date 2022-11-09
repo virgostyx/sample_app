da1d0c339170abca9f8ea1bbcdf6760b7a15bd97
virgostyx <virgostyx@gmail.com>
2022-11-09 13:08:16 +0000

    Change root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  root "static_pages#home"
+
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "application#hello"
 end
