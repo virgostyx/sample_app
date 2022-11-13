40cbbacafe7e1c5c3bc077a60cd343a33cf2623a
virgostyx <virgostyx@gmail.com>
2022-11-13 09:23:42 +0000

    Add User management scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  
   # Defines the root path route ("/")
   root "static_pages#home"
 
+  # Defines the routes for static pages
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
+
+  # Defines the routes for user management
+  get 'users/new'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
