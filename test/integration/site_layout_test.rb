3f2423ddfeda97eaae308f425819c168d06ac1e5
virgostyx <virgostyx@gmail.com>
2022-11-13 09:47:38 +0000

    Finish layout and routes

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -12,5 +12,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
     get contact_path
     assert_select "title", full_title("Contact")
+
+    get signup_path
+    assert_select "title", full_title("Sign up")
+
   end
 end
