b21f516a872c6ebfbaca8532c29581148dff447a
virgostyx <virgostyx@gmail.com>
2022-11-16 16:50:51 +0000

    Add user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,12 @@ class SessionsController < ApplicationController
     
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
+      reset_session
+      log_in user
+      redirect_to user
     else
       # Create an error message.
-      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new', status: :unprocessable_entity
     end
   end
