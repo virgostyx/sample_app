32135e3515ffa91de11c6e3d3e72ac32551e791c
virgostyx <virgostyx@gmail.com>
2022-12-05 17:24:22 +0000

    Add follow/unfllow feature

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,14 @@ Rails.application.routes.draw do
   get "/microposts", to: "static_pages#home"
   
   # Defines resources management
-  resources :users
+  resources :users do
+    member do
+       get :following, :followers
+    end
+  end
+
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
